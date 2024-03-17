26e156b3b9ea46e568e7b0f017668591101d7fe2
ralphsz288 <rakph28@gmail.com>
2024-03-17 20:34:12 +0200

    added uncompleted logic for cookies credentials storage

--- employee-management-client/src/app/core/core.component.ts
+++ employee-management-client/src/app/core/core.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { User } from '../auth/user.model';
 
 @Component({
   selector: 'app-core',
   templateUrl: './core.component.html',
   styleUrl: './core.component.css'
 })
-export class CoreComponent {
-
+export class CoreComponent implements OnInit{
+  constructor(private cookieService: CookieService){}
+  ngOnInit(): void {
+    // this.cookieService.set('user','');
+    console.log(this.cookieService.get('user'));
+    console.log(this.cookieService.get('user') == '');
+    const user = new User (
+      'abc',
+      'ralph',
+      'szakacs',
+      'rakph28@gmail.com',
+      '',
+      'ADMIN',
+  )
+    const userDetails = {
+      user: user,
+      token: 'tokennnn'
+    }
+    this.cookieService.set('user',JSON.stringify(userDetails));
+    console.log(this.cookieService.get('user'));
+  }
 }
